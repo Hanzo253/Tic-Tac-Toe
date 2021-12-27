e81a381c6c2edc5a71490265837200442d575079
David Doan <daviddoan45@gmail.com>
2021-12-27 10:30:07 -0500

    created board and added symbols

---
+++ script.js
@@ -0,0 +1,42 @@
+const boxes = document.querySelectorAll(".box");
+const box1 = document.querySelector(".box-1");
+const box2 = document.querySelector(".box-2");
+const box3 = document.querySelector(".box-3");
+const box4 = document.querySelector(".box-4");
+const box5 = document.querySelector(".box-5");
+const box6 = document.querySelector(".box-6");
+const box7 = document.querySelector(".box-7");
+const box8 = document.querySelector(".box-8");
+const box9 = document.querySelector(".box-9");
+
+// const dialog = document.querySelector(".winner");
+// dialog.returnValue = "The Winner!";
+
+let symbolNum = 0;
+let XorO = true;
+
+const drawSymbol = () => {
+  const symbol = document.querySelector(`.symbol-${symbolNum + 1}`);
+
+  if (XorO) {
+    symbol.innerHTML = `X`;
+    XorO = false;
+  } else {
+    symbol.innerHTML = `O`;
+    XorO = true;
+  }
+
+  // dialog.showModal();
+};
+
+const changeXorO = () => {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("mouseenter", (e) => {
+      symbolNum = i;
+      console.log(symbolNum);
+    });
+    boxes[i].addEventListener("click", drawSymbol);
+  }
+};
+
+changeXorO();
